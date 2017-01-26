c83ace9771cfc39addb28e7df113d5a976433a9f
adaszek <adaszek@gmail.com>
2017-01-26 22:09:53 +0100

    Playing with redis

--- bridge.py
+++ bridge.py
@@ -1,7 +1,9 @@
 import paho.mqtt.client as mqtt
-from time import gmtime, strftime
+import time
 from struct import *
+import redis
 
+r = redis.StrictRedis(host='192.168.1.158', port=6379, db=0)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -15,19 +17,25 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(strftime("%a, %d %b %Y %H:%M:%S")+" "+msg.topic+" "+str(msg.payload))
 
-def on_float(client, userdata, msg):
-    print(strftime("%a, %d %b %Y %H:%M:%S")+" "+msg.topic+" "+str(format(unpack('f', msg.payload)[0], '.1f')))
+def on_temperature(client, userdata, msg):
+    r.zadd('temperature', int(time.time()), str(format(unpack('f', msg.payload)[0], '.1f')))
 
-def on_uint32(client, userdata, msg):
-    print(strftime("%a, %d %b %Y %H:%M:%S")+" "+msg.topic+" "+str(float(unpack('I', msg.payload)[0])/1000))
+def on_humidity(client, userdata, msg):
+    r.zadd('humidity', int(time.time()), str(format(unpack('f', msg.payload)[0], '.1f')))
+
+def on_voltage(client, userdata, msg):
+    r.zadd('voltage', int(time.time()), str(float(format(unpack('I', msg.payload)[0]))/1000))
+
+print(int(time.time()))
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.message_callback_add("r/l/+/s/+/t", on_float)
-client.message_callback_add("r/l/+/s/+/h", on_float)
-client.message_callback_add("r/l/+/s/+/v", on_uint32)
+client.message_callback_add("r/l/+/s/+/t", on_temperature)
+client.message_callback_add("r/l/+/s/+/h", on_humidity)
+client.message_callback_add("r/l/+/s/+/v", on_voltage)
 
 client.connect("192.168.1.157", 1883, 60)
 
