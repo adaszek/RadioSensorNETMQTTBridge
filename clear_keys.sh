bb6f8d0c16fd135f4569fe6640d129ad57659524
adaszek <adaszek@gmail.com>
2017-03-05 16:41:31 +0100

    Devices page show latest activity

--- clear_keys.sh
+++ clear_keys.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-redis-cli -h 192.168.1.158 KEYS "sensor*" | xargs redis-cli -h 192.168.1.158 DEL
+redis-cli -h 192.168.1.158 KEYS "sensor:$1:*" | xargs redis-cli -h 192.168.1.158 DEL
+redis-cli -h 192.168.1.158 SREM "sensors" $1
+redis-cli -h 192.168.1.158 HDEL "sensors:last_location" $1
+redis-cli -h 192.168.1.158 HDEL "sensors:functions" $1
