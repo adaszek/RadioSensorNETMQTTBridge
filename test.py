8339f2b09c2a67c02eb16939e448b8529e425235
adaszek <adaszek@gmail.com>
2017-03-12 21:57:23 +0100

    Thread pools for sensor monitoring added

--- test.py
+++ test.py
@@ -2,25 +2,36 @@ import threading
 import signal
 import time
 import redis
+import math
+
+from itertools import islice
 
 class MonitorThread(threading.Thread):
-    def __init__(self, redis, sensor_id, capabilities, terminate_flag):
+    def __init__(self, redis, capabilities, terminate_flag):
         super().__init__()
         self.redis = redis
-        self.sensor_id = sensor_id
+        self.sensor_ids = capabilities.keys()
         self.capabilities = capabilities
         self.terminate_flag = terminate_flag
 
     def run(self):
         while (not self.terminate_flag.is_set()):
             pipe = self.redis.pipeline()
-            for cap in self.capabilities["r"]:
-                print("sensor:{sid}:{cid}:timestamps".format(sid=self.sensor_id, cid=cap))
-                pipe.zrange("sensor:{sid}:{cid}:timestamps".format(sid=self.sensor_id, cid=cap), -1, -1)
+            for sensor in self.sensor_ids:
+                for cap in self.capabilities[sensor]["r"]:
+                    if cap is not None: 
+                        print("sensor:{sid}:{cid}:timestamps".format(sid=sensor, cid=cap))
+                        pipe.zrange("sensor:{sid}:{cid}:timestamps".format(sid=sensor, cid=cap), -1, -1)
+
+                for cap in self.capabilities[sensor]["w"]:
+                    if cap is not None: 
+                        print("sensor:{sid}:{cid}:timestamps".format(sid=sensor, cid=cap))
+                        pipe.zrange("sensor:{sid}:{cid}:timestamps".format(sid=sensor, cid=cap), -1, -1)
+
             print(pipe.execute())
             self.terminate_flag.wait(5)
 
-        print("killed {sid}".format(sid=self.sensor_id));
+        print("killed {sids}".format(sids=self.sensor_ids));
 
 def get_capabilities(db):
     pipe = db.pipeline()
@@ -48,6 +59,10 @@ def decode_capabilities(to_parse, sensor_list, array_of_cap):
              print("There is no such device as {}".format(k))
     return ret_dict
 
+def dict_chunks(data, size):
+    it = iter(data)
+    for i in range(0, len(data), size):
+        yield {k:data[k] for k in islice(it, size)}
 
 def main():
     flag = threading.Event()
@@ -57,14 +72,25 @@ def main():
         raise SystemExit("Exiting - all threads are being killed")
 
     signal.signal(signal.SIGINT, do_exit)
-    
     r = redis.StrictRedis(host='192.168.1.158', port=6379, db=0, encoding="utf-8", decode_responses=True)
 
-    sids, caps, array_of_cap = get_capabilities(r)
-    sensors = decode_capabilities(caps, sids, array_of_cap)
+    max_number_of_threads = 8
+    number_of_threads = 0;
 
-    for i in sensors:
-        t = MonitorThread(r, i, sensors[i], flag)
+    sids, caps, array_of_cap = get_capabilities(r)
+
+    if len(sids) < max_number_of_threads:
+        number_of_threads = len(sids)
+    else:
+        number_of_threads = max_number_of_threads
+
+    sensors = decode_capabilities(caps, sids, array_of_cap)
+    how_many_per_thread = math.ceil(len(sensors)/number_of_threads)
+
+    print("Number of threads: {} Sensors in db: {} Sensors per thread: {}".format(number_of_threads, len(sensors), how_many_per_thread))
+
+    for sensors_to_process in dict_chunks(sensors, how_many_per_thread):
+        t = MonitorThread(r, sensors_to_process, flag)
         t.start()
 
     main_thread = threading.main_thread()
